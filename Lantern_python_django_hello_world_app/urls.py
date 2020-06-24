227ef6ba60582780bade585244cdfa8fee989486
jdvcDev <jason.cook@microsoft.com>
2020-06-24 16:38:06 -0600

    non template webapp

--- Lantern_python_django_hello_world_app/urls.py
+++ Lantern_python_django_hello_world_app/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+# imported views
+from my_app import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.index, name="homepage")
 ]
